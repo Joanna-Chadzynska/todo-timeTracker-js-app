99cfec87e57108dc08864cf4fffb66cbcd73113b
Joanna Chądzyńska <jmalek87@gmail.com>
2020-02-06 10:34:08 +0100

    added addTask function

--- js/Store.js
+++ js/Store.js
@@ -1,7 +1,20 @@
 class Store {
 	static getTasks() {
-		return fetch(" http://localhost:3000/tasks")
+		return fetch("http://localhost:3000/tasks")
 			.then((resp) => resp.json())
 			.then((data) => data);
+	}
+
+	static addTask(task) {
+		return fetch("http://localhost:3000/tasks", {
+			method: "POST", // or 'PUT'
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(task)
+		})
+			.then((resp) => resp.json())
+			.then((data) => data)
+			.catch((error) => new Error(error));
 	}
 }
